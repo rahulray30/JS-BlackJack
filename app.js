aa0cd38067ec3b3d27da19baddad783c12742a52
rahulray <rayrahul.2011@gmail.com>
2020-12-08 20:45:20 +0530

    JavaScript

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
+let blackjackGame = {
+  you: { scoreSpan: "#your-blackjack-result", div: "#your-box", score: 0 },
+  dealer: {
+    scoreSpan: "#dealer-blackjack-result",
+    div: "#dealer-box",
+    score: 0,
+  },
+};
+
+const YOU = blackjackGame["you"];
+const DEALER = blackjackGame["dealer"];
+
 document
-  .querySelector("#blackjack-stand-btn")
+  .querySelector("#blackjack-hit-btn")
   .addEventListener("click", blackjackHit);
 
+document
+  .querySelector("#blackjack-deal-btn")
+  .addEventListener("click", blackjackDeal);
+
+const hitAudio = new Audio("./pics&sounds/hit-sound.mp3");
+
 function blackjackHit() {
-  console.log("its a hit");
+  showCard(YOU);
 }
 
+function showCard(activePlayer) {
+  let CardImage = document.createElement("img");
+  CardImage.src = "./pics&sounds/10.png";
+  document.querySelector(activePlayer["div"]).appendChild(CardImage);
+  hitAudio.play();
+}
+
+function blackjackDeal() {
+  let allImgYour = document.querySelector("#your-box").querySelectorAll("img");
+  let allImgDealer = document
+    .querySelector("#dealer-box")
+    .querySelectorAll("img");
+
+  for (let i = 0; i < allImgYour.length; i++) {
+    allImgYour[i].remove();
+  }
+  for (let i = 0; i < allImgDealer.length; i++) {
+    allImgDealer[i].remove();
+  }
+}
