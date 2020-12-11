4cdd79a52dd4ba02459af7be192bd720f8bbfdbd
rahulray <rayrahul.2011@gmail.com>
2020-12-11 18:14:35 +0530

    more functionality

--- app.js
+++ app.js
@@ -21,7 +21,14 @@ let blackjackGame = {
     K: 10,
     A: [1, 11],
   },
+  wins: 0,
+  loss: 0,
+  draws: 0,
 };
+
+let hitFlag =0;
+let standFlag = 1;
+let dealFlag =0;
 
 function randomCard() {
   let ran = Math.floor(Math.random() * 13);
@@ -51,7 +58,8 @@ function blackjackHit() {
   showCard(card, YOU);
   updateScore(card, YOU);
   showScore(YOU);
-  console.log(YOU["score"]);
+  standFlag = 0;
+  //console.log(YOU["score"]);
 }
 
 function showCard(card, activePlayer) {
@@ -67,7 +75,22 @@ function blackjackDeal() {
   let allImgDealer = document
     .querySelector("#dealer-box")
     .querySelectorAll("img");
+  if (computeWinner() == YOU) {
+    blackjackGame.wins += 1;
+    console.log(blackjackGame.wins + " wins");
+  } else if (computeWinner() == DEALER) {
+    blackjackGame.loss += 1;
+    console.log(blackjackGame.loss + " losses");
+  } else {
+    blackjackGame.draws += 1;
+  }
 
+  document.querySelector("#wins").textContent = blackjackGame.wins;
+  document.querySelector("#loss").textContent = blackjackGame.loss;
+  document.querySelector("#draws").textContent = blackjackGame.draws;
+
+  document.querySelector("#blackjack-result").textContent = "Let's Play";
+  document.querySelector("#blackjack-result").style.color = "black";
   for (let i = 0; i < allImgYour.length; i++) {
     allImgYour[i].remove();
   }
@@ -80,6 +103,8 @@ function blackjackDeal() {
   document.querySelector(YOU.scoreSpan).style.color = "blanchedalmond";
   document.querySelector(DEALER.scoreSpan).textContent = 0;
   document.querySelector(DEALER.scoreSpan).style.color = "blanchedalmond";
+  hitFlag = 0;
+  standFlag = 1;
 }
 
 function updateScore(cards, activePlayer) {
@@ -105,33 +130,48 @@ function showScore(activePlayer) {
 }
 
 function dealerLogic() {
+  delay();
+  if (DEALER["score"] > 15) {
+    if (computeWinner() == YOU) {
+      document.querySelector("#blackjack-result").textContent = "You won";
+      document.querySelector("#blackjack-result").style.color = "green";
+      //applause audio
+    } else if (computeWinner() == DEALER) {
+      document.querySelector("#blackjack-result").textContent = "You lost";
+      document.querySelector("#blackjack-result").style.color = "red";
+      //lost audio
+    } else {
+      document.querySelector("#blackjack-result").textContent = "It's a draw";
+      document.querySelector("#blackjack-result").style.color = "blue";
+    }
+  }
+}
+
+function delay() {
   let card = randomCard();
   showCard(card, DEALER);
   updateScore(card, DEALER);
   showScore(DEALER);
-  document.querySelector("#blackjack-result").textContent = computeWinner();
-
+  hitFlag = 1;
 }
 
 function computeWinner() {
   let winner;
   if (YOU["score"] <= 21) {
     if (DEALER["score"] > 21) {
-      winner = "You won";
-    } else if (DEALER["score"] <= 21) {
-      if (YOU["scorer"] > DEALER["score"]) {
-        winner = "You won";
-      } else if (YOU["scorer"] == DEALER["score"]) {
-        winner = "Its a draw";
-      } else {
-        winner = "You loss";
-      }
+      winner = YOU;
+    } else if (DEALER["score"] < YOU["score"]) {
+      winner = YOU;
+    } else if (DEALER["score"] > YOU["score"]) {
+      winner = DEALER;
+    } else if (DEALER["score"] == YOU["score"]) {
+      winner = "It's a draw";
     }
   } else if (YOU["score"] > 21) {
     if (DEALER["score"] > 21) {
       winner = "It's a draw";
     } else {
-      winner = "you loss";
+      winner = DEALER;
     }
   }
 
